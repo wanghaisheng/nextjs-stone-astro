6997281664246cb84ab3f059eef4b582ab8b0c4a
luasenvy <luas.envy@gmail.com>
2024-10-07 12:22:40 +0900

    fix: improve `startViewTransition()` more naturally

--- components/ViewTransitionLink.tsx
+++ components/ViewTransitionLink.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link, { type LinkProps } from "next/link";
+import { useRouter } from "next/navigation";
 import type { AnchorHTMLAttributes } from "react";
 
 export type ViewTransitionLinkProps = AnchorHTMLAttributes<HTMLAnchorElement> & LinkProps;
@@ -9,12 +10,20 @@ export default function ViewTransitionLink({
   onClick: handleClick,
   ...props
 }: ViewTransitionLinkProps) {
+  const router = useRouter();
+
   const handleTransition = (e: React.MouseEvent<HTMLAnchorElement>) => {
     if (!document?.startViewTransition) return handleClick?.(e);
 
-    document?.startViewTransition(() => {
-      handleClick?.(e);
-    });
+    const anchor = e.target as HTMLAnchorElement;
+    if (anchor.target !== "_blank") {
+      e.preventDefault();
+      document.startViewTransition(() => {
+        handleClick?.(e);
+
+        router.push(anchor.href ?? "/");
+      });
+    }
   };
 
   return <Link {...props} onClick={handleTransition} />;
